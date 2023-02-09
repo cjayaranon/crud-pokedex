3543b548179b083fc27b12b36b6b51b46322070c
ceej_yaranon <cjayaranon@gmail.com>
2023-02-09 16:19:12 +0800

    Core management command construction - connect to API endpoint and save to db - django admin viewing of records available

--- dexapoke/pokemon/models.py
+++ dexapoke/pokemon/models.py
@@ -1,32 +1,34 @@
 from django.db import models
 
 
-class Pokemons(models.Model):
-    GENDER = (
-        ('MALE', 'Male'),
-        ('FEMALE', 'Female'),
+
+class PokemonType(models.Model):
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(
+        max_length =20,
+        blank = True,
+        null = True
     )
 
-    # SPECIES = (
-    #     ('MALE', 'Male'),
-    #     ('FEMALE', 'Female'),
-    # )
 
-    # ABILITIES = (
-    #     ('MALE', 'Male'),
-    #     ('FEMALE', 'Female'),
-    # )
 
-    # POKEMON_TYPES = (
-    #     ('MALE', 'Male'),
-    #     ('FEMALE', 'Female'),
-    # )
+class PokemonAbility(models.Model):
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(
+        max_length =20,
+        blank = True,
+        null = True
+    )
 
-    # WEAKNESSES = (
-    #     ('MALE', 'Male'),
-    #     ('FEMALE', 'Female'),
-    # )
 
+
+class Pokemons(models.Model):
+    """
+    The Pokemons themselves;
+    related loosely to :model:'PokemonAbility' and
+    :model: 'PokemonType'.
+
+    """
     id = models.AutoField(primary_key = True)
     name = models.CharField(
         max_length = 50,
@@ -34,46 +36,24 @@ class Pokemons(models.Model):
         null = True
     )
     height = models.IntegerField(default=0)
+    
     weight = models.IntegerField(default=0)
-    gender = models.CharField(
-        max_length = 7,
-        choices = GENDER,
-        blank = True
-    )
-    category = models.CharField(
-        max_length = 12,
-        # choices = SPECIES,
-        blank = True,
-        null = True
-    )
-    abilities = models.CharField(
-        max_length = 12,
-        # choices = ABILITIES,
-        blank = True,
-        null = True
-    )
 
+    # cannot use ForeignKey because there can be multiple instances of these
+    # objects in one Pokemon
     pokemon_type = models.CharField(
-        max_length = 12,
-        # choices = POKEMON_TYPES,
+        max_length = 80,
         blank = True,
         null = True
     )
 
-    weaknesses = models.CharField(
-        max_length = 12,
-        # choices = WEAKNESSES,
+    pokemon_ability = models.CharField(
+        max_length = 80,
         blank = True,
         null = True
     )
 
-# Name (text)
-# NDEX# (auto_id)
-# Height (integer)
-# Weight (integer)
 # Gender (choice)
 # Category (choice)
-# Abilities (choice)
-# Type (choice)
 # Weaknesses (choice)
 # Evolutions (choice; needs more info)
